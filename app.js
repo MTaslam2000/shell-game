d9de65221f2b9c3d12df5e141a2b6183576da680
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2023-01-10 14:10:19 -0600

    got DOM elements

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 /* Imports */
 
 /* Get DOM Elements */
+const brownBtn = document.getElementById('brown-button');
+const whiteBtn = document.getElementById('white-button');
+const snailBtn = document.getElementById('snail-button');
+
+const brownContainer = document.getElementById('brown-container');
+const whiteContainer = document.getElementById('white-container');
+const snailContainer = document.getElementById('snail-container');
+
+const winsEL = document.getElementById('wins');
+const totalEl = document.getElementById('total');
 
 /* State */
 
